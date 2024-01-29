83ec27a5f573a20595eef04486eeeb80c9d6d708
Andres Felipe Escobar <anfelesvillada@gmail.com>
2024-01-29 11:42:39 -0500

    Pruebas parte 2
    se crean los test para order y order activity

--- orderweb/database/seeders/DatabaseSeeder.php
+++ orderweb/database/seeders/DatabaseSeeder.php
@@ -46,6 +46,8 @@ class DatabaseSeeder extends Seeder
         Technician::factory(2)->create();*/
 
         //$this->call(TestTechnicianSeeder::class);
-        $this->call(TestActivitySeeder::class);
+        //$this->call(TestActivitySeeder::class);
+        $this->call(TestOrderSeeder::class);
+        //$this->call(TestOrderActivitySeeder::class);
     }
 }
